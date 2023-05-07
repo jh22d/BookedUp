88221362b07148b3913b03e586a5326431bd1d12
jh22d <jh0220x@gmail.com>
2023-05-07 16:56:40 -0700

    tidy

--- alter.py
+++ alter.py
@@ -1,9 +1,9 @@
 # If the cell `shap_values = explainer.shap_values(X_enc_df)` takes too much time to run, please try either:
-# - Clone the repository of this project by navigating to Terminal and call `git clone https://github.com/jh22d/XU-LEE-econ323-proj`. Get back to this file and uncomment & run the 3 code cells below.
+# - Clone the repository of this project by navigating to Terminal and call `git clone https://github.com/jh22d/BookedUp.git`. Get back to this file and uncomment & run the 3 code cells below.
 
 # or
 
-# - Go to [the repository of this project](https://github.com/jh22d/XU-LEE-econ323-proj), navigate to the `results/` folder and check the force plots manually.
+# - Go to [the repository of this project](https://github.com/jh22d/BookedUp), navigate to the `results/` folder and check the force plots manually.
 
 # And comment out the 3 code cells below that generate the force plots containing the function `shap.force_plot`.
 
